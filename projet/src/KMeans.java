f28ec584412044168c646d6c201cda6f20caf1e7
Célie Ponroy <celie.ponroy@laposte.net>
2024-06-19 15:57:05 +0200

    fin kmeans

--- projet/src/KMeans.java
+++ projet/src/KMeans.java
@@ -1,118 +1,81 @@
-import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
-public class KMeans{
+public class KMeans implements Algo{
 
-    private int k;
     private int maxIterations;
 
-    //private int[][] data;
-    private List<List<Position>> centroids;
+    //private List<List<Pixel>> clusters;
+    List<Color> couleurs;
+    NormeCouleurs distanceCouleur;
+    private int[]  clusters;
 
-
-
-    public KMeans(int k, int maxIterations) {
-        this.k = k;
+    public KMeans(int maxIterations,List<Color> couleurs, NormeCouleurs normeCouleurs) {
         this.maxIterations = maxIterations;
-        this.centroids = new ArrayList<List<Position>>();
+        this.couleurs = couleurs;
+        this.distanceCouleur = normeCouleurs;
+
     }
+    public int[] algo(int[][] pixels) {
+        //initialisation
+        int taillepix = pixels.length;
 
-
-    public void algo(List<Color> couleurs, NormeCouleurs distanceCouleur, String path) throws IOException {
-        BufferedImage image = ImageIO.read(new File(path));
-        centroids = new ArrayList<List<Position>>();
-        k = couleurs.size();
-        for (int i = 0; i< k; i++){
-            centroids.add(new ArrayList<Position>());
-        }
         boolean end = false;
-        Palette palette = new Palette(couleurs, distanceCouleur);
+        int iteration = 0;
         while(!end){
-            for(int i = 0; i < image
-        }
-    }
+            Palette palette = new Palette(couleurs, distanceCouleur); //comme couleurs change on le reinitialise
 
+            //initialisation des groupes
+            clusters = new int[taillepix];
 
-
-    /*
-    private void initializeCentroids() {
-        Random random = new Random();
-        for (int i = 0; i < k; i++) {
-            int randomIndex = random.nextInt(data.length);
-            centroids[i] = data[randomIndex];
-        }
-    }
-
-    private int closestCentroid(int[] pixel) {
-        int closestIndex = 0;
-        double minDistance = Double.MAX_VALUE;
-        for (int i = 0; i < k; i++) {
-            double distance = euclideanDistance(pixel, centroids[i]);
-            if (distance < minDistance) {
-                minDistance = distance;
-                closestIndex = i;
+            iteration++;
+            System.out.println(iteration);
+            ArrayList<ArrayList<Color>> clustersColor = new ArrayList<>();
+            for (int i = 0; i< couleurs.size(); i++){
+                clustersColor.add(new ArrayList<Color>());
             }
-        }
-        return closestIndex;
-    }
-
-    private void recomputeCentroids(int[] labels) {
-        int[][] newCentroids = new int[k][3];
-        int[] counts = new int[k];
-
-        for (int i = 0; i < labels.length; i++) {
-            int cluster = labels[i];
-            newCentroids[cluster][0] += data[i][0];
-            newCentroids[cluster][1] += data[i][1];
-            newCentroids[cluster][2] += data[i][2];
-            counts[cluster]++;
-        }
-
-        for (int i = 0; i < k; i++) {
-            if (counts[i] != 0) {
-                centroids[i][0] = newCentroids[i][0] / counts[i];
-                centroids[i][1] = newCentroids[i][1] / counts[i];
-                centroids[i][2] = newCentroids[i][2] / counts[i];
+            for (int i =0; i<pixels.length;i++) {
+                int[] pixel = pixels[i];
+                Color colorPixel= new Color(pixel[0],pixel[1],pixel[2]);
+                Color proche = palette.getPlusProche(colorPixel);
+                //on mets la bonne valeur
+                
+                int indexCouleur = couleurs.indexOf(proche);
+                clusters[i] = indexCouleur;
+                clustersColor.get(indexCouleur).add(colorPixel);
             }
-        }
-    }
-
-    private double euclideanDistance(int[] a, int[] b) {
-        return Math.sqrt(Math.pow(a[0] - b[0], 2) + Math.pow(a[1] - b[1], 2) + Math.pow(a[2] - b[2], 2));
-    }
-
-    public int[][] run() {
-        initializeCentroids();
-        int[] labels = new int[data.length];
-
-        for (int iteration = 0; iteration < maxIterations; iteration++) {
-            // Assign clusters
-            for (int i = 0; i < data.length; i++) {
-                labels[i] = closestCentroid(data[i]);
+            //on recalcule les centroides
+            List<Color> nvCouleurs= new ArrayList<Color>();
+            for (List<Color>  list : clustersColor ) {
+                nvCouleurs.add(moyenneColor(list));
             }
+            //si les anciens centoides = nouveau end = true 
+            if(nvCouleurs.equals(couleurs))
+                end = true;
+            else
+                couleurs = nvCouleurs;
+            //si iteration = maxIterations end = true
+            if(iteration == maxIterations)
+                end = true;
 
-            // Recompute centroids
-            recomputeCentroids(labels);
-
-            // Convergence check can be added here
         }
-
-        // Assign final cluster colors to pixels
-        int[][] clusteredPixels = new int[data.length][3];
-        for (int i = 0; i < data.length; i++) {
-            clusteredPixels[i] = centroids[labels[i]];
-        }
-        return clusteredPixels;
+        return clusters;
     }
-
-     */
-
-
+    public Color moyenneColor(List<Color> liste) {
+        if(liste.size()==0){
+            return null;
+        }
+        int[] moyen = {0,0,0};
+        for (Color color : liste) {
+            int[] tab = OutilCouleur.getTabColor(color.getRGB());
+            moyen[0] += tab[0];
+            moyen[1] += tab[1];
+            moyen[2] += tab[2];   
+        }
+        int tailleliste = liste.size();
+        
+        return new Color(moyen[0]/tailleliste,moyen[1]/tailleliste,moyen[2]/tailleliste);
+    }
 
 }
--- projet/src/MainTest2.java
+++ projet/src/MainTest2.java
@@ -9,22 +9,49 @@ import java.util.List;
 public class MainTest2 {
 
     public static void main(String[] args) {
-        /*
+        
         try {
-            BufferedImage image = ImageIO.read(new File("./projet/src/img.png"));
-            int k = 5; // nombre de clusters
+            BufferedImage image = ImageIO.read(new File("/home/celie/Documents/s4/MethodeOptimisation/SAE_01.1_amaglio_bitsindou_ponroy/projet/img/Planete 1.jpg"));
             int maxIterations = 100; // nombre maximum d'itérations
+            List<Color> couleurs = PaletteBiome.DEFAULT.biome;
+            System.out.println(PaletteBiome.DEFAULT.biome.size());
 
-            int[][] pixelData = Algo.getPixelData(image);
-            KMeans kMeans = new KMeans(k, maxIterations, pixelData);
-            int[][] clusteredPixels = kMeans.run();
 
-            BufferedImage outputImage = Algo.createClusteredImage(image, clusteredPixels);
-            ImageIO.write(outputImage, "jpg", new File("copie.jpg"));
+            KMeans kMeans = new KMeans(maxIterations,PaletteBiome.DEFAULT.biome,new NormeCIELAB());
+            
+            System.out.println(kMeans.moyenneColor(couleurs));            
+
+            int[][] pixels = new int[image.getWidth()*image.getHeight()][3];
+
+            int o =0;
+            for (int i = 0; i < image.getWidth(); i++) {
+                for (int j = 0; j < image.getHeight(); j++) {
+                    int[] rgb = OutilCouleur.getTabColor(image.getRGB(i, j));
+                    pixels[o][0]=rgb[0];
+                    pixels[o][1]=rgb[1];
+                    pixels[o][2]=rgb[2];
+                    o++;  
+                }
+            }
+            int[] res = kMeans.algo(pixels);
+
+            int width = image.getWidth();
+            int height =  image.getHeight();
+    
+            BufferedImage image2 = new BufferedImage(width, height,BufferedImage.TYPE_3BYTE_BGR);
+            int x=0;
+            for(int i=0;i<width;i++){
+                for(int j = 0 ; j<height;j++){
+                    image2.setRGB(i, j, couleurs.get(res[x]).getRGB());
+                    x++;
+                }
+            }
+            ImageIO.write(image2, "JPG", new File("./resultats/clusterTEST.png"));
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-         */
+         
     }
 }
